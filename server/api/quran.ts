6f5a300fbbe42980f8c57e4d05b97eaecdeda822
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-08-10 06:56:58 +0300

    new trial 10

--- server/api/quran.ts
+++ server/api/quran.ts
@@ -1,13 +1,14 @@
 import { H3Event } from "h3";
 import quran from "~/server/db/static/quran.json";
-
+import { _reParseString } from "waelio-utils";
 export default defineEventHandler((event: H3Event) => {
   const query = getQuery(event);
   const index = query && query.index ? +query.index : 0;
   const Size = quran.length;
   const legitIndex = index && index <= Size
-  const Data = legitIndex ? quran[index-1] : quran
+  const response = legitIndex ? quran[index - 1] : quran
   return {
-    data: Data
+    // @ts-expect-error
+    data: _reParseString(response)
   }
 })
