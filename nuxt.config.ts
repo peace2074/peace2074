246da5b846ceed9dc4cba9126a8d1bf2f78c3bd5
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-08-10 06:43:43 +0300

    new trial 9

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,8 @@ export default defineNuxtConfig({
   app: {
     head: {
       htmlAttrs: { lang: 'en' },
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
       meta: [
         { charset: 'utf-8' }
       ],
