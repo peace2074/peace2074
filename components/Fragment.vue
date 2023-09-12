d4447913b02c9addb51df51b480147f354e15c06
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-09-12 21:51:52 +0300

    new test

--- components/Fragment.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div :class="autoClass">
-    <v-slot />
-  </div>
-</template>
-<script setup lang="ts">
-import { /*isDark, useToggle, autoStyle, */autoClass } from "../mixins";
-</script>
