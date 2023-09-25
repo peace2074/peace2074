ce70fc9b7023bca8062a1d4d242b4882d2e96648
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2023-09-25 16:27:22 +0300

    working arabic

--- pages/quran/sura/[id].vue
+++ pages/quran/sura/[id].vue
@@ -1,40 +1,40 @@
 <script lang="ts" setup>
-import { ref, watchEffect, useRoute, onBeforeMount } from '#imports';
 import { useQuran } from '../../../composables/useQuran'
+import { core } from '../../../utils/core'
+import { onBeforeMount, ref, useRoute, watchEffect } from '#imports'
+
 const { start, _quran, _legends, _enums } = useQuran()
-import { core } from "../../../utils/core";
 
 const route = useRoute()
 const sura = ref(1)
 
-
-const target = !!_quran ? _quran.value.find(s => s.index == sura.value.toString()) : ''
+const target = computed(() => _quran.value.find(s => s.index === sura.value.toString()))
 
 watchEffect(() => {
   sura.value = +route.params.id
 })
 onBeforeMount(() => {
-  start();
-  if (core.has(_enums.quran)) {
+  start()
+  if (core.has(_enums.quran))
     _quran.value = core.getItem(_enums.quran)
-  }
-  if (core.has(_enums.legend)) {
+
+  if (core.has(_enums.legend))
     _quran.value = core.getItem(_enums.legend)
-  }
 })
 </script>
+
 <template>
   <ClientOnly>
     <div>
-      {{ sura }}
-    </div>
-    <div>
       <div>
-        <div>index: {{ target.index }}</div>
-        <div> name: {{ target.name }}</div>
-        <div v-for="one in target.aya"> <span>{{ one.text }}</span> </div>
+        <div>رقم السوره: {{ target.index }}</div>
+        <div> اسم السوره: {{ target.name }}</div>
+        <div v-for="one in target.aya">
+          <span>{{ one.text }}</span>
+        </div>
       </div>
     </div>
   </ClientOnly>
 </template>
+
 <style></style>
