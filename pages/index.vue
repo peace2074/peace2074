05b8c47fab8cb496c913861a207cb72d8e29fb0b
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2023-10-02 06:54:08 +0300

    workig audio

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,16 @@
 <script setup lang="ts">
 import { useSound } from '@vueuse/sound'
 
-const audioList = "/assets/raw/Blue.mp3"
+const audioList = 'https://cdn.islamic.network/quran/audio/128/ar.alafasy/262.mp3'
 // const { data } = await useFetch(audioList)
 // import { useI18n } from "#imports"
-// const { data } = useFetch('http://api.alquran.cloud/v1/ayah/262/ar.alafasy')
+const { data } = useFetch('http://api.alquran.cloud/v1/ayah/262/ar.alafasy')
 
 const playbackRate = 0.75
 
-
-
-
-const { play, stop } = useSound(audioList)
-
 const options = {
   autoplay: true,
-  playbackRate
+  playbackRate,
 }
 </script>
 
@@ -23,9 +18,15 @@ const options = {
   <KeepAlive>
     <PageView :views="true">
       <Logos mx-auto mb-6 class="table" aria-label="logos" />
-      <q-btn label="play" @click="play" />
-      <q-btn label="stop" @click="() => stop(0)" />
-      <pre>{{ play }}</pre>
+      <audio controls autoplay>
+        <source :src="audioList" type="audio/ogg">
+        <source :src="audioList" type="audio/mpeg">
+        Your browser does not support the audio element.
+      </audio>
+      <div class="text-h5">
+        {{ data.data.text }}
+      </div>
+      <!-- <pre>{{ data }}</pre> -->
     </PageView>
   </KeepAlive>
 </template>
