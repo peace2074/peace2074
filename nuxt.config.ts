a3840b0ce90080b2793eb0aad1ee8fdc60f71150
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-12-21 20:30:50 +0200

    stage 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
-import { pwa } from './config/pwa'
-import { appDescription } from './constants/index'
+import { pwa } from "./config/pwa";
+import { appDescription } from "./constants/index";
 
 export default defineNuxtConfig({
   modules: [
-    '@vueuse/nuxt',
-    '@unocss/nuxt',
-    '@pinia/nuxt',
-    '@nuxtjs/color-mode',
-    '@vite-pwa/nuxt',
+    "@vueuse/nuxt",
+    "@unocss/nuxt",
+    "@pinia/nuxt",
+    "@nuxtjs/color-mode",
+    "@vite-pwa/nuxt",
+    "nuxt-quasar-ui",
   ],
 
   experimental: {
@@ -19,39 +20,43 @@ export default defineNuxtConfig({
     typedPages: true,
   },
 
-  css: [
-    '@unocss/reset/tailwind.css',
-  ],
+  css: ["@unocss/reset/tailwind.css"],
 
   colorMode: {
-    classSuffix: '',
+    classSuffix: "",
   },
 
   nitro: {
     esbuild: {
       options: {
-        target: 'esnext',
+        target: "esnext",
       },
     },
     prerender: {
       crawlLinks: false,
-      routes: ['/'],
-      ignore: ['/hi'],
+      routes: ["/"],
+      ignore: ["/hi"],
     },
+  },
+  quasar: {
+    /* */
   },
 
   app: {
     head: {
-      viewport: 'width=device-width,initial-scale=1',
+      viewport: "width=device-width,initial-scale=1",
       link: [
-        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: "icon", href: "/favicon.ico", sizes: "any" },
+        { rel: "icon", type: "image/svg+xml", href: "/nuxt.svg" },
+        { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
       ],
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: appDescription },
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "description", content: appDescription },
+        {
+          name: "apple-mobile-web-app-status-bar-style",
+          content: "black-translucent",
+        },
       ],
     },
   },
@@ -61,4 +66,4 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true,
   },
-})
+});
