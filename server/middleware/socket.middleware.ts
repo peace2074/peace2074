bda8027a334cab73d843edf7d30d214a07bec313
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-12-29 15:26:47 +0200

    chat io

--- server/middleware/socket.middleware.ts
+++ server/middleware/socket.middleware.ts
@@ -1,37 +1,19 @@
+// server/middleware/socket.ts
 import type { Socket } from 'socket.io'
 import { Server } from 'socket.io'
 
-const appSocket = {
-  emit: (channel: string, message: string) => {
-    console.log('Not initiated yet', channel, message)
-  },
-}
-
-export default defineEventHandler((event) => {
-  event.context.appSocket = appSocket
-
+export default defineEventHandler(({ node }) => {
   if (global.io)
     return
-  console.log('Initiating socket.middleware')
 
-  const node = event.node
   global.io = new Server(node.res.socket?.server)
-  global.io.on('connection', (socket: Socket) => {
-    socket.emit('message-channel', `welcome ${socket.id}`)
 
-    listeners.push({ channel: 'message', socket })
-
-    appSocket.emit = (channel, message) => {
-      global.io.emit(channel, message)
-    }
+  global.io.on('connection', async (socket: Socket) => {
+    console.log('a user connected')
 
     socket.on('message', (data) => {
-      console.log('Relaying again for funsies: ', data)
-      global.io.emit('message-channel', `Hello, client! ${data}`)
-    })
-
-    socket.on('disconnect', () => {
-      // Put optional disconnect logic here
+      console.log('received message:', data)
+      global.io.emit('message', 'Hello, client!')
     })
   })
 })
